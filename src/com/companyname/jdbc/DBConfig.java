1b8c01cbf98ff13485212bfbeb6e8352d338bf31
ahdieh <aemadi@my.centennialcollege.ca>
2015-12-13 17:05:59 -0500

    Initial commit - connected to database

---
+++ src/com/companyname/jdbc/DBConfig.java
@@ -0,0 +1,25 @@
+package com.companyname.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+// CONFIGURATION UTILITY CLASS
+public class DBConfig {
+	// PRIVATE CONSTANTS
+	private static final String USERNAME = "ahdieh";
+	private static final String PASSWORD = "123456";
+	private static final String CONN_STRING = "jdbc:mysql://localhost/moviedb";
+
+	// UTILTITY METHOD that connects us to the mysql database
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+	}
+	
+	// UTILITY METHOD that displays our errors in more detail
+	public static void displayException(SQLException exception) {
+		System.err.println("Error Message: " + exception.getMessage());
+		System.err.println("Error Code: " + exception.getErrorCode());
+		System.err.println("SQL State: " + exception.getSQLState());
+	}
+}
